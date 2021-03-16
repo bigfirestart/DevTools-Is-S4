bf2eae38d04a15e8b63d824292398d421f8e6000
Mark <bigfirestart@gmail.com>
2021-03-16 12:57:39 +0300

    feat(added doc sample)

--- lab1/Devtools-xcode-lab1/Devtools-xcode-lab1/main.swift
+++ lab1/Devtools-xcode-lab1/Devtools-xcode-lab1/main.swift
@@ -4,6 +4,11 @@ import Foundation
 
 //MARK - <#name#>
 
+/// Basic calculator documentation
+/// - Parametrs
+///   - a: First number
+///   - b: Second number
+///   - symb: One of "+", "-", "/", "*" symbols
 func calc (a: Int, b: Int, symb: String) -> Float{
     switch symb {
     case "*":
@@ -22,5 +27,24 @@ var a = Int(readLine() ?? "0")!
 var symb = readLine()!
 var b = Int(readLine() ?? "1")!
 
+
+/**
+ Creates a personalized greeting for a recipient.
+
+ - Parameter recipient: The person being greeted.
+
+ - Throws: `MyError.invalidRecipient`
+           if `recipient` is "Derek"
+           (he knows what he did).
+
+ - Returns: A new string saying hello to `recipient`.
+ */
+func greeting(to recipient: String) -> String {
+    guard recipient != "Derek" else {
+        return ""
+    }
+
+    return "Greetings, \(recipient)!"
+}
 
 print(calc(a: a, b: b, symb: symb))
