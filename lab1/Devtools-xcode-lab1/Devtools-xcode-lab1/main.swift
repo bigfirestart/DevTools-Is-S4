f42478eb0b0553e602db18843d36b47db73d481c
Mark <bigfirestart@gmail.com>
2021-03-16 10:37:23 +0300

    feat(lab1 parts 1-4)

---
+++ lab1/Devtools-xcode-lab1/Devtools-xcode-lab1/main.swift
@@ -0,0 +1,23 @@
+import Foundation
+
+
+func calc (a: Int, b: Int, symb: String) -> Float{
+    switch symb {
+    case "*":
+        return Float(a*b)
+    case "/":
+        return Float(a/b)
+    case "+":
+        return Float(a+b)
+    case "-":
+        return Float(a-b)
+    default:
+        return  0
+    }
+}
+var a = Int(readLine() ?? "0")!
+var symb = readLine()!
+var b = Int(readLine() ?? "1")!
+
+
+print(calc(a: a, b: b, symb: symb))
---
+++ lab1/Devtools-xcode-lab1/lab1-tests/lab1_tests.swift
@@ -0,0 +1,42 @@
+//
+//  lab1_tests.swift
+//  lab1-tests
+//
+//  Created by Кирилл Лукьянов on 15.03.2021.
+//
+
+import XCTest
+import Foundation
+@testable import Devtools_xcode_lab1
+
+class lab1TestsSum: XCTestCase {
+    func test() {
+        XCTAssertEqual(calc(a: 1, b:2, symb: "+"), Float(3))
+        XCTAssertEqual(calc(a: -1, b:2, symb: "+"), 1)
+        XCTAssertEqual(calc(a: -10, b:-202, symb: "+"), -212)
+    }
+}
+
+class lab1TestsDec: XCTestCase{
+    func test(){
+        XCTAssertEqual(calc(a: 2, b:2, symb: "-"), 0)
+        XCTAssertEqual(calc(a: 11, b:1, symb: "-"), 10)
+        XCTAssertEqual(calc(a: -10, b:-202, symb: "-"), 192)
+    }
+}
+
+class lab1TestsMult: XCTestCase{
+    func test(){
+        XCTAssertEqual(calc(a: 1, b:2, symb: "*"), 2)
+        XCTAssertEqual(calc(a: -1, b:2, symb: "*"), -2)
+        XCTAssertEqual(calc(a: -10, b:-202, symb: "*"), 2020)
+    }
+}
+
+class lab1TestsDev: XCTestCase{
+    func test(){
+        XCTAssertEqual(calc(a: 1, b:2, symb: "/"), Float(1/2))
+        XCTAssertEqual(calc(a: 10, b:2, symb: "/"), 5)
+        XCTAssertEqual(calc(a: -10, b:10, symb: "/"), -1)
+    }
+}
